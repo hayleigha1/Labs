64395592039ae1e1b4a3db47eb02d398c7320f88
hayleigha1 <113569909+hayleigha1@users.noreply.github.com>
2022-09-28 21:28:26 -0500

    Add files via upload
    Do some finishing touches

--- lab05.py
+++ lab05.py
@@ -1,9 +1,9 @@
 ########################################################################
 ##
 ## CS 101 Lab
-## Program #
-## Name
-## Email
+## Program # Week 6; Lab 5
+## Name: Hayleigh Arnold
+## Email: hca8qp@umsystem.edu
 ##
 ## PROBLEM : Describe the problem
 ##
@@ -18,100 +18,100 @@
 ##
 ########################################################################
 
-#import modules needed
-import random
 
-def play_again() -> bool:
-    ''' Asks the user if they want to play again, returns False if N or NO, and True if Y or YES.  Keeps asking until they respond yes '''
-    user_input = input("Do you want to play again? ==>")
+# import statements
+import string
 
-    while (user_input.upper() != "YES") and (user_input.upper() != "Y") and (user_input.upper() != "NO") and (user_input.upper() != "NO"):
-        print("Try again.")
-        user_input = input("Do you want to play again?")
-    if (user_input.upper() == "YES") or (user_input.upper() == "Y"):
-        return True
-    elif (user_input.upper() == "NO") or (user_input.upper() == "N"):
-        return False
-     
-def get_wager(bank : int) -> int:
-    ''' Asks the user for a wager chip amount.  Continues to ask if they result is <= 0 or greater than the amount they have '''
-    wager = int(input("How many chips do you want to wager? ==> "))
+# functions
+def character_value(char):
+    value = ord(char)
+    value = value - 65
+    return value
 
-    while (wager < 0) or (wager > bank):
-        if(wager < 0):
-            print("The wager amount must be greater than 0.  Please enter again. ")
-            wager = int(input("How many chips do you want to wager? ==> "))
-        elif(wager > bank):
-            print("The wager amount cannot be greater than how much you have. ", bank)
-            wager = int(input("How many chips do you want to wager? ==> "))
+def get_school(string): 
+    if string[5] == "1":
+        return "School of Computing and Engineering SCE"
+    elif string[5] == "2":
+        return "School of Law"
+    elif string[5] == "3":
+        return "College of Arts and Sciences"
+    else:
+        return "Invalid School"
 
-    return wager
+def get_grade (library_card):
+    if library_card[6] == '1':
+        return "Freshman"
+    elif library_card[6] == '2':
+        return "Sophomore"
+    elif library_card[6] == '3':
+        return "Junior"
+    elif library_card[6] == '4':
+        return "Senior"
+    else:
+        return "Invalid Grade"
 
-def get_slot_results() -> tuple:
-    ''' Returns the result of the slot pull '''
+def get_check_digit(library_card):
+    sum = 0
+    for i in range (len(library_card)):
+        value = character_value(library_card[i])
+        sum += (i + 1)*value
+    total = sum
+    check_digit = sum%10
+    return check_digit
+
+def verify_check_digit(library_card):
+    for i in range (0,5):
+        if len(library_card) != 10:
+            return False, "The length of the number given must be 10"
+
+    for i in range (0,5):
+        if (ord(library_card[i]) < 65) or (ord(library_card[i]) > 90):
+            error = str(library_card[i])
+            i = str(i)
+            return False, "The first 5 characters must be A-Z, the invalid character is at " + i + " is " + error
+
+    for i in range(7,10):
+        if (library_card[i] < '0') or (library_card[i] > '9'):
+            error = str(library_card[i])
+            i = str(i)
+            return False, "The last 3 characters must be 0-9, the invalid character is at " + i + " is " + error
+        else:
+            return True, ""
+
+    for i in range(6):
+        if (library_card[5] != "1") and (library_card[5] != "2") and (library_card[5] != "3"):
+
+            return False, "The sixth character must be 1 2 or 3"
+
+    for i in range(7):
+        if (library_card[6] != "1") or (library_card[6] != "2") or (library_card[6] != "3") or (library_card[6] != "4"):
+            return False, "The seventh character must be 1 2 or 3"
+    for i in range (10):
+        if library_card[9] != get_check_digit(library_card):
+            error = str(library_card[9])
+            correct = str(get_check_digit(library_card))
+            return False, "Check Digit " + error + " does not match calculated value " + correct + " ."
+        else:
+            return True, ""
     
-
-    return random.randint(1,10),random.randint(1,10),random.randint(1,10)
-
-def get_matches(reela, reelb, reelc) -> int:
-    ''' Returns 3 for all 3 match, 2 for 2 alike, and 0 for none alike. '''
-    if (reela == reelb) and (reela == reelc):
-        return 3
-    elif (reela == reelc) and (reela != reelb):
-        return 2
-    elif (reela == reelb) and (reela != reelc):
-        return 2
-    elif (reelb == reelc) and (reelb != reela):
-        return 2
-    else:
-        return 0
-
-def get_bank() -> int:
-    ''' Returns how many chips the user wants to play with.  Loops until a value greater than 0 and less than 101 '''
-    bank = int(input("How many chips do you want to start with? ==> "))
-
-    while (bank < 0) or (bank > 100):
-        if (bank < 0):
-            print("Too low a value, you can only choose 1 - 100 chips ")
-            bank = int(input("How many chips do you want to start with? ==> "))
-        elif (bank > 100):
-            print("Too high a value, you can only choose 1 - 100 chips ")
-            bank = int(input("How many chips do you want to start with? ==> "))
+    
+    
+    
         
-    return bank
-
-def get_payout(wager, matches):
-    ''' Returns how much the payout is.. 10 times the wager if 3 matched, 3 times the wager if 2 match, and negative wager if 0 match '''
-    if matches == 3:
-        return (wager * 10)
-    elif matches == 2:
-        return (wager * 3)
-    else:
-        return (wager * -1)
+    
+    
+    
 
 if __name__ == "__main__":
 
-    playing = True
-    while playing:
-
-        bank = get_bank()
-
-        while bank > 0:  # Replace with condition for if they still have money.
-            
-            wager = get_wager(bank)
-
-            reel1, reel2, reel3 = get_slot_results()
-
-            matches = get_matches(reel1, reel2, reel3)
-            payout = get_payout(wager, matches)
-            bank = bank + payout
-
-            print("Your spin", reel1, reel2, reel3)
-            print("You matched", matches, "reels")
-            print("You won/lost", payout)
-            print("Current bank", bank)
-            print()
-           
-        print("You lost all", 0, "in", 0, "spins")
-        print("The most chips you had was", 0)
-        playing = play_again()
+    # main program
+    print(f'{"Linda Hall":^60}')
+    print(f'{"Library Card Check":^60}')
+    print("==" * 30)
+    
+    
+    
+    library_card = input("Enter Library Card.  Hit Enter to Exit ==> ")
+    
+    
+    
